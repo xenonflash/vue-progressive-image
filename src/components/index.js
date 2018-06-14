502b360ea5b97cf769d79270c120258e83649d00
yankechao <yankechao@vipkid.com.cn>
2018-06-14 11:26:24 +0800

    改成插件形式

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,8 @@
 import Com from './progressive-image.vue'
-export default Com
+const pImage = {
+  install: function(Vue, options) {
+    //TODO, 注册全局的 handleScroll queue 来处理 需要监听的元素
+    Vue.component(Com.name, Com)
+  }
+}
+export default pImage
--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import ProgressiveImage from './components/progressive-image'
+import ProgressiveImage from './components/index.js'
 import './css/normalize.css'
-Vue.component(ProgressiveImage.name, ProgressiveImage)
-
+// Vue.component(ProgressiveImage.name, ProgressiveImage)
+Vue.use(ProgressiveImage)
 new Vue({
   el: '#app',
   render: h => h(App)
